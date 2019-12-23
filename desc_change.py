7681d46adc110be80515058fef415aa1594a0b5e
Ari Mendelow <ari@mendelow.me>
2019-12-22 21:12:26 -0800

    Update script to run on all products in the store

--- desc_change.py
+++ desc_change.py
@@ -83,48 +83,44 @@ def ChangeProdDesc(prodId, desc):
   return(request.json())
 
 result = GetFirstProduct()
-
-product = result["data"]["products"]
-hasNextPage = product["pageInfo"]["hasNextPage"]
-cursor = product["edges"][0]["cursor"]
-prodNode = product["edges"][0]["node"]
-descriptionOrig = prodNode["descriptionHtml"]
-prodId = prodNode["id"]
-title = prodNode["title"]
-
 counter = 1
-print(f"{counter}: {title}")
-# print(description)
 
-# Rules for altering the description
-changes = [
-  ("a.m.e. is proud", "We are proud")
-]
+hasNextPage = True
+while(hasNextPage):
+  product = result["data"]["products"]
+  hasNextPage = product["pageInfo"]["hasNextPage"]
+  cursor = product["edges"][0]["cursor"]
+  prodNode = product["edges"][0]["node"]
+  descriptionOrig = prodNode["descriptionHtml"]
+  prodId = prodNode["id"]
+  title = prodNode["title"]
 
-descriptionNew = descriptionOrig
-for change in changes:
-  toAlter = change[0]
-  alteration = change[1]
-  if(re.search(toAlter, descriptionOrig, flags=re.I)):
-    print(f"Found match for '{toAlter}'...")
-    descriptionNew = re.sub(toAlter, alteration, descriptionOrig, flags=re.I)
+  print(f"{counter}: {title}")
 
-if (descriptionNew != descriptionOrig):
-  print("Sending updated description...")
-  result = ChangeProdDesc(prodId, descriptionNew)
-  print("Done.")
-else:
-  print("No changes made!")
+  # Rules for altering the description
+  changes = [
+    ("a.m.e. is proud", "We are proud")
+  ]
 
-# while(hasNextPage):
-#   result = GetNextProduct(cursor)
+  descriptionNew = descriptionOrig
+  for change in changes:
+    toAlter = change[0]
+    alteration = change[1]
+    if(re.search(toAlter, descriptionOrig, flags=re.I)):
+      print(f"Found match for '{toAlter}'...")
+      descriptionNew = re.sub(toAlter, alteration, descriptionOrig, flags=re.I)
 
-#   product = result["data"]["products"]
-#   hasNextPage = product["pageInfo"]["hasNextPage"]
-#   cursor = product["edges"][0]["cursor"]
-#   prodNode = product["edges"][0]["node"]
-#   description = prodNode["descriptionHtml"]
-#   prodId = prodNode["id"]
-#   title = prodNode["title"]
-#   counter += 1
-#   print(f"{counter}: {title}")
+  if (descriptionNew != descriptionOrig):
+    print("Sending updated description...")
+    result = ChangeProdDesc(prodId, descriptionNew)
+    print("Done.")
+  else:
+    print("No changes made!")
+
+  print()
+
+  if(hasNextPage):
+    counter += 1
+    result = GetNextProduct(cursor)
+  else:
+    print(f"Finished going through all {counter} products!")
